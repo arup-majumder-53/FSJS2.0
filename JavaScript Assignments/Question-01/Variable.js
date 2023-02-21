569b6aa63e8b131bca87b4e68a6ec677ce1d1b6a
arup-majumder-53 <arupmajumder053@gmail.com>
2023-02-22 00:18:30 +0530

    updating

--- JavaScript Assignments/Question-01/Variable.js
+++ JavaScript Assignments/Question-01/Variable.js
@@ -6,5 +6,16 @@ let isStudent =  false; // boolean data type
 let hobbies = undefined;//undefined data type 
 let temparature = null; //null data type
 
-console.log("my name is", myName ,"and the data type is" + typeof(myName));
+console.log("my name is", myName ,"and the data type is", typeof(myName));
+console.log("the value of the 2nd variable is", isStudent ,"and the data type is", typeof(isStudent));
+console.log("the value of the 3rd variable is", hobbies ,"and the data type is", typeof(hobbies));
+console.log("the value of the 4th variable is", temparature ,"and the data type is", typeof(temparature));
+
+// output
+// my name is arup and the data type is string
+// the value of the 2nd variable is false and the data type is boolean
+// the value of the 3rd variable is undefined and the data type is undefined
+// the value of the 4th variable is null and the data type is object
+
+
 
