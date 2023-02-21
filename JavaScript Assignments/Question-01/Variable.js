916cde8cdca1f30a2a6ca202479518cdfd16137c
arup-majumder-53 <arupmajumder053@gmail.com>
2023-02-21 23:59:43 +0530

    initial commit

---
+++ JavaScript Assignments/Question-01/Variable.js
@@ -0,0 +1,10 @@
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types,
+// Display all the value with their data type.
+
+let myName = "arup"; //string data type
+let isStudent =  false; // boolean data type
+let hobbies = undefined;//undefined data type 
+let temparature = null; //null data type
+
+console.log("my name is", myName ,"and the data type is" + typeof(myName));
+
