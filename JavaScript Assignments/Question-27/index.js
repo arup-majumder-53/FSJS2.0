85351919e4254e4b87f5e36244e6fa5cfcd3d0d5
arup-majumder-53 <arupmajumder053@gmail.com>
2023-03-14 23:58:30 +0530

    updating

--- JavaScript Assignments/Question-27/index.js
+++ JavaScript Assignments/Question-27/index.js
@@ -1,2 +1,49 @@
 // Write a program to print all the prime number between 0 to 100 (0 and 100 included).
 
+function printprime() {
+
+    for (let i = 0; i <= 100; i++) {
+      let isPrime = true;
+      for (let j = 2; j < i; j++) {
+          if (i % j == 0) {
+            isPrime = false;
+              break;
+          }
+      }
+      if ( i > 1 && isPrime ) {
+          console.log(i);
+      }
+  }
+    
+  }
+  
+  printprime();
+
+
+
+// output
+// 2
+// 3
+// 5
+// 7
+// 11
+// 13
+// 17
+// 19
+// 23
+// 29
+// 31
+// 37
+// 41
+// 43
+// 47
+// 53
+// 59
+// 61
+// 67
+// 71
+// 73
+// 79
+// 83
+// 89
+// 97
